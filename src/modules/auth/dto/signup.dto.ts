7b3eebd6ef9ea4a6b7890388f5bdecbe9378cc42
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-02-10 08:30:51 +0600

    Setup Server And Database

---
+++ src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+} from 'class-validator';
+
+export class SignUpDto {
+    @ApiProperty({ example: 'Mohammad Jihad' })
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @ApiProperty({ example: 'user@gmail.com' })
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({ example: '0178221212121' })
+    @IsNotEmpty()
+    @IsString()
+    phone: string;
+
+    @ApiProperty({ example: '12345678' })
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+};
