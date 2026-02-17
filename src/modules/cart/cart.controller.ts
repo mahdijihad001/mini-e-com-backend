304dd156a0c2292e83db5effad93d2295d1d37a0
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-02-17 12:50:26 +0600

    complite full add to cart api

---
+++ src/modules/cart/cart.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { CartService } from './cart.service';
+import { ApiBearerAuth, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt.auth.guards';
+import { AddToCartDto } from './dto/add.to.cart.dto';
+
+@Controller('cart')
+export class CartController {
+  constructor(private readonly cartService: CartService) { }
+
+  @Post("add-to-cart")
+  @ApiOperation({ summary: "Product Add to cart" })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  async addToCart(@Body() dto: AddToCartDto, @Req() req: any) {
+    const userId = req.user.id;
+
+    await this.cartService.addCart(dto, userId);
+
+    return {
+      success: true,
+      message: "Product added to cart"
+    }
+  }
+
+  @Get("my-cart-list")
+  @ApiOperation({ summary: "My all cart list" })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  async myCartList(@Req() req: any) {
+    const userId = req.user.id;
+
+    const result = await this.cartService.getMyCart(userId);
+
+    return {
+      success: true,
+      data: result
+    }
+
+  }
+
+  @Delete("remove-item/:id/cart")
+  @ApiOperation({ summary: "Remove item from own cart" })
+  @ApiBearerAuth()
+  @ApiParam({ name: "id", example: "cart Id" })
+  @UseGuards(JwtAuthGuard)
+  async removeItemFromCart(@Param("id") cartId: string, @Req() req: any) {
+
+    const userId = req.user.id;
+
+    await this.cartService.removeItemFromMyCart(cartId, userId);
+
+    return {
+      success: true,
+      message: "Item remove from cart"
+    }
+
+  }
+
+
+
+}
